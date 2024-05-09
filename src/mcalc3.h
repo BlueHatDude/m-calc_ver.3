1434ae370b798a52463b6893746220e820c5c74b
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-05-09 10:49:13 -0400

    starting over

--- src/mcalc3.h
+++ src/mcalc3.h
@@ -1,11 +1,5 @@
 #ifndef MCALCULATOR_VER3_H_
 #define MCALCULATOR_VER3_H_
 
-    typedef enum MC3_ErrorCode {
-        MC3_Error_NO_ERROR,
-        MC3_,
-    } MC3_ErrorCode;
-
-    extern double MC3_evaluateEqu(const char* equ);
 
 #endif /* MCALCULATOR_VER3_H_ */
--- src/tests.c
+++ src/tests.c
@@ -2,6 +2,5 @@
 
 
 int main(void) {
-    double result = MC3_evaluateEqu("2 + 4");
-    (void) result;
+
 }
