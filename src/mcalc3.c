82da38a318c3d3a5c21f0aa83aa7244ce86642ef
BluehatAspie <ehmdiagne0530@gmail.com>
2024-08-19 05:49:29 -0400

    refractored tokenize()

--- src/mcalc3.c
+++ src/mcalc3.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
-#include <stdio.h>
+#include <stdio.h> /* only for debugging purposes */
 
 
 typedef float float32_t;
@@ -15,7 +15,7 @@ typedef double float64_t;
 #define MAX_TOKENS 50
 
 
-typedef enum TokenType {
+enum TokenType {
 
     /* operators */
     OP_ADD,
@@ -44,12 +44,12 @@ typedef enum TokenType {
     CONSTANT_PI,
     CONSTANT_E
 
-} TokenType;
+};
 
 
 typedef struct EquToken {
     
-    TokenType type;
+    enum TokenType type;
     
     union {
         int64_t ivalue;
@@ -86,7 +86,7 @@ static void initTokens(EquToken tokens[], const size_t size) {
 }
 
 
-static inline void setToken(EquToken* tkn, TokenType type, double value) {
+static inline void setToken(EquToken* tkn, enum TokenType type, double value) {
     tkn->type = type;
     if (type == TYPE_INTEGER) {
         tkn->ivalue = (int64_t) value;
@@ -118,95 +118,113 @@ static MC3_ErrorCode addToken(const EquToken* ref, EquToken tkns[], size_t* pos,
 }
 
 
-static MC3_ErrorCode tokenize(const char* equ, EquToken tkns[], const size_t max) {
+static MC3_ErrorCode read_number(const char* equ, EquToken* tkn, size_t* index) {
+    const size_t j = (*index);
+    bool is_decimal = false;
+    while (isdigit(equ[*index]) || equ[*index] == '.') {
+        if (equ[*index] == '.')
+            is_decimal = true;
+        
+        (*index)++;
+    }
+
+    if (is_decimal) {
+        setToken(tkn, TYPE_DECIMAL, strtod(&equ[j], NULL));
+    } 
+    else {
+        setToken(tkn, TYPE_INTEGER, strtol(&equ[j], NULL, 10));
+    }
+
+    /* prevents position pointer from skipping over character after number */
+    (*index)--;
+
+    return NO_ERROR;
+}
+
+static bool is_operator(char c) {
+    switch (c) {
+        case '+': case '-': case '*': case '/': case '^':
+            return true;
+        default:
+            return false;
+    }
+}
+
+
+static enum TokenType char_to_type(char c) {
+    switch (c) {
+        case '+': return OP_ADD;
+        case '-': return OP_SUB;
+        case '*': return OP_MULT;
+        case '/': return OP_DIV;
+        case '^': return OP_EXP;
+        default:  return TYPE_EMPTY;
+    }
+}
+
+
+static bool is_grouping(char c) {
+    return (c == '(') || (c == ')');
+}
+
+
+/**
+ * @brief tokenizes string `equ` and reads into `tkns`
+ * 
+ * @param equ 
+ * @param tkns 
+ * @param tkns_size 
+ * @return MC3_ErrorCode 
+ */
+static MC3_ErrorCode tokenize(const char* equ, EquToken tkns[], const size_t tkns_size) {
     EquToken ref_token;
     size_t pos = 0;
     const size_t EQU_LENGTH = strlen(equ);
 
     for (size_t i = 0; i < EQU_LENGTH; i++) {
-        switch (equ[i]) {
-            /* operators */
-            case '+':
-                setToken(&ref_token, OP_ADD, 0);
-                break;
-            case '-':
-                setToken(&ref_token, OP_SUB, 0);
-                break;
-            case '*':
-                setToken(&ref_token, OP_MULT, 0);
-                break;
-            case '/':
-                setToken(&ref_token, OP_DIV, 0);
-                break;
-            case '^':
-                setToken(&ref_token, OP_EXP, 0);
-                break;
-            /* parenthesis */
-            case '(':
+        /* checking for numbers, operators, and grouping */
+        if (is_operator(equ[i])) {
+            enum TokenType type = char_to_type(equ[i]);
+            setToken(&ref_token, type, 0);
+        } else if (is_grouping(equ[i])) {
+            if (equ[i] == '(')
                 setToken(&ref_token, PAR_LEFT, 0);
-                break;
-            case ')':
+            else
                 setToken(&ref_token, PAR_RIGHT, 0);
-                break;
-            /* checking for numbers */
-            case '0': case '1': case '2': case '3': case '4':
-            case '5': case '6': case '7': case '8': case '9': {
-                const int j = (int) i;
-                bool is_decimal = false;
-                while (isdigit(equ[i]) || equ[i] == '.') {
-                    if (equ[i] == '.')
-                        is_decimal = true;
-                    
-                    i++;
-                }
-
-                if (is_decimal) {
-                    setToken(&ref_token, TYPE_DECIMAL, strtod(&equ[j], NULL));
-                } 
-                else {
-                    setToken(&ref_token, TYPE_INTEGER, strtol(&equ[j], NULL, 10));
-                }
-
-                /* prevents position pointer from skipping over character after number */
-                i--;
-                break; 
-            }
-            default: {
-                /* checking for functions */
-                // TODO: remove unecessary iterations through words
-                if (stringAt("sin", &equ[i])) {
-                    setToken(&ref_token, FN_SIN, 0);
-                }
-                else if (stringAt("cos", &equ[i])) {
-                    setToken(&ref_token, FN_COS, 0);
-                } 
-                else if (stringAt("tan", &equ[i])) {
-                    setToken(&ref_token, FN_TAN, 0);
-                } 
-                else if (stringAt("log", &equ[i])) {
-                    setToken(&ref_token, FN_LOG_10, 0);
-                } 
-                else if (stringAt("ln", &equ[i])) {
-                    setToken(&ref_token, FN_LOG_E, 0);
-                }
-                /* checking for constants */
-                else if (stringAt("pi", &equ[i])) {
-                    setToken(&ref_token, CONSTANT_PI, 0);
-                }
-                else if (stringAt("e", &equ[i])) {
-                    setToken(&ref_token, CONSTANT_E, 0);
-                }
-                else {
-                    ref_token.type = TYPE_EMPTY;
-                }
-
-                break;
-            }
+        } else if (isdigit(equ[i])) {
+            read_number(equ, &ref_token, &i);
+        }
+        /* checking for functions */
+        else if (stringAt("sin", &equ[i])) {
+            setToken(&ref_token, FN_SIN, 0);
+        }
+        else if (stringAt("cos", &equ[i])) {
+            setToken(&ref_token, FN_COS, 0);
+        } 
+        else if (stringAt("tan", &equ[i])) {
+            setToken(&ref_token, FN_TAN, 0);
+        } 
+        else if (stringAt("log", &equ[i])) {
+            setToken(&ref_token, FN_LOG_10, 0);
+        } 
+        else if (stringAt("ln", &equ[i])) {
+            setToken(&ref_token, FN_LOG_E, 0);
+        }
+        
+        /* checking for constants */
+        else if (stringAt("pi", &equ[i])) {
+            setToken(&ref_token, CONSTANT_PI, 0);
+        }
+        else if (stringAt("e", &equ[i])) {
+            setToken(&ref_token, CONSTANT_E, 0);
+        }
+        else {
+            ref_token.type = TYPE_EMPTY;
         }
 
         /* adding token to tokens array */
         if (ref_token.type != TYPE_EMPTY) {
-            addToken(&ref_token, tkns, &pos, max);
+            addToken(&ref_token, tkns, &pos, tkns_size);
         }
     }
 
@@ -214,15 +232,26 @@ static MC3_ErrorCode tokenize(const char* equ, EquToken tkns[], const size_t max
 }
 
 
+/**
+ * @brief creates parse tree using tokens
+ * 
+ * @param tkns 
+ * @param size 
+ * @return MC3_ErrorCode 
+ */
+static MC3_ErrorCode parse_tokens(EquToken tkns[], const size_t size);
+
+
 void DEBUGGING_printToken(const EquToken tkn);
 void DEBUGGING_printTokens(const EquToken tkns[], const size_t size);
 void DEBUGGING_printCompactTokens(const EquToken tkns[]);
 
 
-double MC3_evaluate(const char *equ) {
+double MC3_evaluate(const char *equ, MC3_ErrorCode* err) {
     /* declare requisite variables */
     MC3_ErrorCode errorCode = NO_ERROR;
     (void) errorCode;
+    (void) err;
     EquToken tokens[MAX_TOKENS];
     initTokens(tokens, MAX_TOKENS);
     
