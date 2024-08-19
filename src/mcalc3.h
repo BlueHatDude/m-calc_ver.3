6093381a80f1d936f1540d50ea29bdf451baae9f
BluehatAspie <ehmdiagne0530@gmail.com>
2024-08-19 05:49:20 -0400

    changed MC3_evaluate signature

--- src/mcalc3.h
+++ src/mcalc3.h
@@ -7,6 +7,6 @@
         TOKENS_LIMIT_REACHED
     } MC3_ErrorCode;
 
-    extern double MC3_evaluate(const char* equ);
+    extern double MC3_evaluate(const char* equ, MC3_ErrorCode* err);
 
 #endif /* MCALCULATOR_VER3_H_ */
--- src/tests.c
+++ src/tests.c
@@ -1,14 +1,15 @@
 #include "mcalc3.h"
+#include <stddef.h>
 
 
-void testTokenization(void) {
-    MC3_evaluate("2 + 4 - 6 * 8 / 10 ^ 12");
-    MC3_evaluate("8 * (2 + 4)");
-    MC3_evaluate("sin(pi / 2)");
-    MC3_evaluate("ln(e ^ 5)");
+void test_tokenization(void) {
+    MC3_evaluate("2 + 4 - 6 * 8 / 10 ^ 12", NULL);
+    MC3_evaluate("8 * (2 + 4)", NULL);
+    MC3_evaluate("sin(pi / 2)", NULL);
+    MC3_evaluate("ln(e ^ 5)", NULL);
 }
 
 
 int main(void) {
-
+    test_tokenization();
 }
