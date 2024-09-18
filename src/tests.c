772610243be7898a83f7348c2c59104c64e6027f
BluehatAspie <ehmdiagne0530@gmail.com>
2024-09-18 18:15:52 -0400

    added asserts

--- src/tests.c
+++ src/tests.c
@@ -1,12 +1,21 @@
 #include "mcalc3.h"
 #include <stddef.h>
+#include <assert.h>
 
 
 void test_tokenization(void) {
-    MC3_evaluate("2 + 4 - 6 * 8 / 10 ^ 12", NULL);
-    MC3_evaluate("8 * (2 + 4)", NULL);
-    MC3_evaluate("sin(pi / 2)", NULL);
-    MC3_evaluate("ln(e ^ 5)", NULL);
+    MC3_ErrorCode error_code = MC3_NO_ERROR;
+
+    MC3_evaluate("2 + 4 - 6 * 8 / 10 ^ 12", &error_code);
+    assert(error_code == MC3_NO_ERROR);
+    MC3_evaluate("8 * (2 + 4)", &error_code);
+    assert(error_code == MC3_NO_ERROR);
+    MC3_evaluate("sin(pi / 2)", &error_code);
+    assert(error_code == MC3_NO_ERROR);
+    MC3_evaluate("ln(e ^ 5)", &error_code);
+    assert(error_code == MC3_NO_ERROR);
+    MC3_evaluate("2x * 5", &error_code);
+    assert(error_code == MC3_INVALID_CHARACTER_FOUND);
 }
 
 
