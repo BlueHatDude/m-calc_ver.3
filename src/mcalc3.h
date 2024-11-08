92d27aceccaff1fcdbc9b1164f0d4f98f8cd47a5
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2024-11-08 11:40:14 -0500

    added func

--- src/mcalc3.h
+++ src/mcalc3.h
@@ -11,4 +11,8 @@
     extern double MC3_evaluate(const char* equ, MC3_ErrorCode* err);
     extern const char* getErrorString(const MC3_ErrorCode err);
 
+    static inline int error_has_occured(MC3_ErrorCode* err) {
+        return (*err != MC3_NO_ERROR);
+    }
+
 #endif /* MCALCULATOR_VER3_H_ */
